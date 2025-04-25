599422dbcf1178403328e2d7acc7bf5b44766020
Gambled23 <ipog71@gmail.com>
2025-04-24 19:39:03 -0600

    feat: links

--- Gambled/Gambled.py
+++ Gambled/Gambled.py
@@ -44,7 +44,7 @@ def sidebar_items() -> rx.Component:
         sidebar_item("Home", "slash", "/"),
         sidebar_item("Bio", "book-text", "/bio"),
         sidebar_item("Portfolio", "folder-kanban", "/portfolio"),
-        sidebar_item("Social", "link", "/social"),
+        sidebar_item("Links", "link", "/links"),
         sidebar_item("Resumé", "file-user", "/Resume.pdf", True),
         spacing="1",
         width="100%",
@@ -287,9 +287,99 @@ def bio():
 def portfolio():
     return rx.text("Portfolio")
 
-@rx.page(title="Social")
-def social():
-    return rx.text("Social")
+@rx.page(title="Links")
+def links():
+        return rx.flex(
+        rx.color_mode.button(position="top-right"),
+        sidebar(),
+        rx.vstack(
+            rx.heading("Bio", 
+                        as_="h1", 
+                        size="9",
+                        width="100%",
+                        align="center",
+                    ),
+            rx.container(
+                rx.heading(
+                    "Contact",
+                    text_align="left",
+                ),
+                rx.list.unordered(
+                    rx.list.item(
+                        rx.link(
+                            "Email",
+                            href="mailto:ipog71@gmail.com",
+                            is_external=True,
+                        )
+                    ),
+                    rx.list.item(
+                        rx.link(
+                            "Telegram",
+                            href="https://t.me/Gambled23",
+                            is_external=True,
+                        )
+                    ),
+                    padding_bottom="2em",
+                ),
+
+                rx.heading(
+                    "Social",
+                    text_align="left",
+                ),
+                rx.list.unordered(
+                    rx.list.item(
+                        rx.link(
+                            "Mastodon",
+                            href="https://mastodon.social/@Gambled23",
+                            is_external=True,
+                        )
+                    ),
+                    rx.list.item(
+                        rx.link(
+                            "Discord",
+                            href="https://discord.com/users/320649011000246272",
+                            is_external=True,
+                        )
+                    ),
+                    padding_bottom="2em",
+                ),
+
+                rx.heading(
+                    "Media logging",
+                    text_align="left",
+                ),
+                rx.list.unordered(
+                    rx.list.item(
+                        rx.link(
+                            "Books",
+                            href="https://www.goodreads.com/user/show/113163810-c-sar-daniel-gonz-lez",
+                            is_external=True,
+                        )
+                    ),
+                    rx.list.item(
+                        rx.link(
+                            "Movies",
+                            href="https://www.imdb.com/user/ur102644810/ratings?ref_=nv_usr_rt_4",
+                            is_external=True,
+                        )
+                    ),
+                    rx.list.item(
+                        rx.link(
+                            "Anime",
+                            href="https://anilist.co/user/Gambled23/",
+                            is_external=True,
+                        )
+                    ),
+                    padding_bottom="2em",
+                ),
+                width="100%",
+            ),
+            align="center",
+            width="100%",
+            padding_top="4em",
+            padding_right="2em",
+        ),
+    )
 
 app = rx.App()
 app.add_page(index)
