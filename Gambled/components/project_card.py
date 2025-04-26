73951a8479bc26a3ceb08c2d1e84dc0528566e39
Gambled23 <ipog71@gmail.com>
2025-04-26 12:11:44 -0600

    refactor: project component

--- Gambled/components/project_card.py
+++ Gambled/components/project_card.py
@@ -1,25 +1,9 @@
 import reflex as rx
-
-class ImageState(rx.State):
-    images = [
-        "/logos/github.png",
-        "/logos/gmail.png",
-        "/logos/nix.png",
-    ]
-    current_index: int = 0
-
-    @rx.event
-    def next_image(self):
-        self.current_index = (self.current_index + 1) % len(self.images)
+from typing import Optional, Dict, Any
 
 def project_card(
-          avatar: str,
-          title: str,
-          description_short: str,
-          description_long: str,
-          link: str = None,
-          repo: str = None,
-          size: str = "3",
+        project: Optional[Dict[str, Any]] = None,
+        size: str = "3",
 
 ) -> rx.Component:
     return rx.dialog.root(
@@ -27,10 +11,10 @@ def project_card(
         rx.card(
             rx.link(
             rx.flex(
-                rx.avatar(src=avatar),
+                rx.avatar(src=project["avatar"]),
                 rx.box(
-                    rx.heading(title),
-                    rx.text(description_short,),
+                    rx.heading(project["title"],),
+                    rx.text(project["description_short"],),
                 ),
                 height="100%",
                 align="center",
@@ -43,33 +27,40 @@ def project_card(
         ),
     ),
     rx.dialog.content(
-        rx.dialog.title(title),
+        rx.dialog.title(project["title"]),
         rx.dialog.description(
-            description_long, 
+            project["description_long"], 
             white_space="pre-wrap",
             font_size="1.1em",
             text_align="justify",
         ),
-        rx.image(
-            src=ImageState.images[ImageState.current_index]
+        rx.cond(
+            project["body_image"],
+            rx.image(
+                src=project["body_image"],
+                width="100%",
+                height="auto",
+                border_radius="20px",
+                padding="0.5em",
+                margin_x="auto",
+            ),
         ),
-        rx.moment(interval=3000, on_change=ImageState.next_image, display="none"),
         rx.hstack(
             rx.cond(
-                link,
+                project["link"],
                 rx.link(
                     rx.button(
                         rx.icon("square-arrow-out-up-right"),
                         "Link",
                     ),
-                    href=link,
+                    href=project["link"],
                     is_external=True,
                     color_scheme="green",
                     variant="solid",
                 ),
             ),
             rx.cond(
-                repo,
+                project["repo"],
                 rx.link(
                     rx.button(
                         rx.icon("github"),
@@ -78,7 +69,7 @@ def project_card(
                     color="white",
                     color_scheme="blue",
                     variant="solid",
-                    href=repo,
+                    href=project["repo"],
                     is_external=True,
                 ),
             ),
--- Gambled/variables.py
+++ Gambled/variables.py
@@ -1,1 +1,33 @@
 age: int = 21
+
+projects = {
+    "asanawave": {
+        "avatar":"/asanawave/logo.svg",
+        "title":"Asanawave",
+        "description_short":"Webapp django app for a yoga teacher",
+        "description_long":"Currently I’m working with a three people team to develop and deploy a webapp for a private client, using Django as backend and HTMX to replace Javascript in the frontend.\nWe provide a space for a private teachers to upload lessons and manage all of their learning materials, while keeping close communication with their students.\nMy main responsibilities as a developer are integrating newly developed components to the system, write tests for existing components, and as a server administrator to provide a seamless integration on cloud.",
+        "link":"https://asanawave.com/",
+        "repo":"",
+        "body_image":"/asanawave/1.png",
+    },
+
+    "nix-dotfiles": {
+        "avatar":"/logos/nix.png",
+        "title":"Nix dotfiles",
+        "description_short":"My nix configuration for all my devices",
+        "description_long":"All of my devices run NixOS, so I decided to create a repository with all of my dotfiles and configuration files.\nI use this repository to keep track of all the changes I make to my system, and to share my configuration with others.\nFeel free to snoop around.",
+        "link":"",
+        "repo":"https://github.com/Gambled23/nix-dotfiles",
+        "body_image":"",
+    },
+
+    "dixios": {
+        "avatar":"/logos/ia.svg",
+        "title":"Dixios",
+        "description_short":"AI and LLM Consultant at Dixios",
+        "description_long":"I'm currently working as a consultant for Dixios, a company that gives training to office employees on how to use AI tools to improve their productivity.\nMy main responsibilities are to create training materials, and to give training sessions to employees.\nI also help the company to integrate AI tools into their workflow, and to create custom solutions for their clients.\n\nFun fact: I worked as a fullstack dev for the webpage too.",
+        "link":"https://dixios.com/",
+        "repo":"",
+        "body_image":"/dixios/1.png",
+    },
+}
