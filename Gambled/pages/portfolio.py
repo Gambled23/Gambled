3d560fc8a8db12be2c871a2ee960a9f44e5948fd
Gambled23 <ipog71@gmail.com>
2025-04-26 15:35:37 -0600

    feat: project logos

--- Gambled/pages/portfolio.py
+++ Gambled/pages/portfolio.py
@@ -1,6 +1,19 @@
 import reflex as rx
 from Gambled.components.sidebar import sidebar
-from Gambled.variables import *
+from Gambled.components.project_card import project_card
+from Gambled.variables import projects
+
+def subtitle(
+    text: str,
+) -> rx.Component:
+    return rx.heading(
+        text, 
+        as_="h3", 
+        size="8",
+        color=rx.color("mint", 12),
+        align="left",
+        padding_top="1em",
+    ),
 
 @rx.page(title="Portfolio")
 def portfolio():
@@ -8,13 +21,54 @@ def portfolio():
         # rx.color_mode.button(position="top-right"),
         sidebar(),
         rx.vstack(
-            rx.heading("Portfolio", 
-                        as_="h1", 
+            rx.flex(
+                rx.heading("Portfolio",
+                        as_="h1",
                         size="9",
-                    ),
-            align="center",
+                        align="center",
+                ),
             width="100%",
-            padding_top="4em",
-            padding_right="2em",
+            direction="column",
+            spacing="3",
+            ),
+            subtitle("Web developer"),
+            rx.grid(
+                project_card(project=projects["asanawave"]),
+                project_card(project=projects["nisha"]),
+                project_card(project=projects["dixios"]),
+                project_card(project=projects["OnlineWeddingInvitation"]),
+                project_card(project=projects["Gambled23"]),
+                columns=rx.breakpoints(sm="1", md="3",),
+                spacing="4",
+                padding_x="1em",
+                width="100%",
+            ),
+
+            subtitle("Personal projects"),
+            rx.grid(
+                project_card(project=projects["TheParkingZone"]),
+                project_card(project=projects["nix-dotfiles"]),
+                columns=rx.breakpoints(sm="1", md="3",),
+                spacing="4",
+                padding_x="1em",
+                width="100%",
+            ),
+
+            rx.heading(
+                "Discord bots",
+                as_="h4", 
+                size="6",
+                color=rx.color("mint", 12),
+                align="left",
+            ),
+            rx.grid(
+                project_card(project=projects["eliasbot"]),
+                project_card(project=projects["caphpibot"]),
+                columns=rx.breakpoints(sm="1", md="3",),
+                spacing="4",
+                padding_x="1em",
+                width="100%",
+            ),
+        width="100%",
         ),
     ),
--- Gambled/variables.py
+++ Gambled/variables.py
@@ -34,8 +34,8 @@ projects = {
         "technologies":["html", "css", "bootstrap", "webhosting",],
     },
 
-    "Nisha": {
-        "avatar":"/nisha/logo.png",
+    "nisha": {
+        "avatar":"/nisha/logo.ico",
         "title":"Nisha",
         "description_short":"Crochet and knitting e-commerce",
         "description_long":"Nisha is a small e-commerce that sells crochet plushies and knitted decorations.\nHere I worked as a fullstack webdev creating a cozy yet elegant experience for the users browsing the store.\nEverything here was selfhosted on a rpi4 using nginx and mariadb.\nSadly, the store is no longer active, but you can still check the code on the repository.",
@@ -46,7 +46,7 @@ projects = {
     },
 
     "eliasbot": {
-        "avatar":"/eliasbot/logo.png",
+        "avatar":"/eliasbot/logo.svg",
         "title":"Eliasbot",
         "description_short":"Discord bot for a private server",
         "description_long":"Self hosted on a raspberry pi, I made this bot using Hikari, a python API for Discord bots, it have some funny commands like quote the bible, birthday celebrations, calling all members, and even play some music using the spotify api.\nJust a silly project I made for my discord server.",
@@ -57,7 +57,7 @@ projects = {
     },    
     
     "caphpibot": {
-        "avatar":"/caphpibot/logo.png",
+        "avatar":"/caphpibot/logo.svg",
         "title":"Caphpibot",
         "description_short":"ElCapibe's discord bot",
         "description_long":"My second time building a discord bot, this time using Laracord, a Laravel forked microframework for building discord bots.\nThis bot was used to manage 'La capibanda', El Capibe's league of legends community server\nIt had commands to interact with the influencer, vote for next video ideas, play games in discord, connect your League of legends account, and some moderation commands to automate the server.\nWhen the server grew too big, I decided to stop maintaining the bot and the moderators switched to other automod bots, but it was a fun experience.\n",
@@ -68,9 +68,9 @@ projects = {
     },
 
     "TheParkingZone": {
-        "avatar":"",
+        "avatar":"/theparkingzone/logo.ico",
         "title":"The Parking Zone",
-        "description_short":"GUI for complete parking lot administration",
+        "description_short":"GUI for complete parking lot administration, including QR code I/O and ticket printing",
         "description_long":"A system for parking-spaces administration, auto-assigning the nearest available space, and printing a QR code with the assigned space.\nMade entirely using python, this project uses tkinter to give the admin an easy to use GUI\nAs an administrator, you can watch live status of the parking lot, receive notifications from cars parked for more than 48 hours, create and delete parking spaces, and everything you would expect from a parking lot administration software.\nThe software runs entirely on the local PC using a self-hosted postgres database, but thanks to the GUI the administrator user doesn't need to know anything about databases to fully use The parking zone.",
         "link":"",
         "repo":"https://github.com/Gambled23/The-Parking-Zone",
@@ -79,9 +79,9 @@ projects = {
     },
 
     "OnlineWeddingInvitation": {
-        "avatar":"",
+        "avatar":"/wedding/logo.svg",
         "title":"Online Wedding Invitation",
-        "description_short":"",
+        "description_short":"Hosted online wedding invitation for a private client",
         "description_long":"A project for a private client, the client wanted to have an online invitation for their wedding, so they could send the link to everyone invited instead of waiting for the cards to arrive to each guest.\nI deployed for a private client a modified version of dewanakl’s online wedding invitation, the invitation was fully accesible on the web until after the wedding day\nIt has a countdown for the wedding day and some important details about the event like the location or exact time.\nThe client was very happy with the result, and I was happy to help them with their wedding.",
         "link":"",
         "repo":"https://github.com/Gambled23/undangan",
@@ -90,7 +90,7 @@ projects = {
     },
 
     "Gambled23": {
-        "avatar":"/logo.png",
+        "avatar":"/favicon.ico",
         "title":"Personal website",
         "description_short":"You're looking at it!",
         "description_long":"My personal webpage, I made this using pure python thanks to Reflex\nThis started like an experiment of me trying to recreate my (now old) personal landingpage buit with just html and css, but without actually writing any html or css.\nThis project helped me learn a lot about reflex, and how you can use it to create a fullstack webapp at a really good speed.\n\nAnd now as I write this the card reloads and words appear on the screen, some wizardy happens here.\n",
